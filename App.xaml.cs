1e4d6f9c6f110ac3a0617bdd1b5639c428ed3f1a
Rayan Dongol <113275485+chirayanmabu@users.noreply.github.com>
2024-01-07 08:20:16 +0545

    initial commiit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace hawa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace hawa
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
